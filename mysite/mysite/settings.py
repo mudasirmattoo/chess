8040b28e033ee10e996d37c73244d7649ef1b6be
Mudasir Mushtaq <120006081+mudasirmattoo@users.noreply.github.com>
2024-09-05 22:55:29 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
